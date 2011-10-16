9e6ece5e071e594785c184e69d343c56370c2d16
sa <sanami77@gmail.com>
2011-10-16 22:48:49 +0300

    Delete action for found files

--- app/form.rb
+++ app/form.rb
@@ -3,10 +3,13 @@ Qt::require ROOT('resources/resources.qrc'), ROOT('tmp')
 
 class Form < Qt::MainWindow
 	slots 'on_toolButton_clicked()'
+	slots 'on_toolButton_2_clicked()'
+	slots 'on_toolButton_3_clicked()'
 	slots 'on_comboBox_currentIndexChanged(const QString &)'
 	slots 'on_checkBox_clicked()'
 
 	slots 'on_action_start_triggered()'
+	slots 'on_action_delete_files_triggered()'
 
 	slots 'on_action_quit_triggered()'
 	slots 'on_action_new_triggered()'
@@ -69,8 +72,14 @@ private
 		#end
 		#@ui.groupBox.dispose
 
-		@ui.treeWidget.setColumnWidth 0, 600
+		@ui.treeWidget.setColumnWidth 0, 150
 		@ui.treeWidget.setColumnWidth 1, 600
+		@ui.treeWidget.setColumnWidth 2, 600
+
+		@ui.treeWidget_2.setColumnWidth 0, 150
+
+		# Контекстное меню
+		@ui.treeWidget_2.addAction @ui.action_delete_files
 
 	end
 
@@ -86,13 +95,6 @@ private
 		statusBar.showMessage str
 	end
 	
-	def inform_pic_change(pic)
-		@settings.current_file = pic.filename
-		t = Time.now.strftime('%H:%M:%S')
-		log "#{t} #{pic.columns} x #{pic.rows} #{pic.filesize} #{pic.filename}"
-		show_message "#{t} #{pic.filename}"
-	end
-
 	##
 	# Выйти из программы
 	def on_action_quit_triggered
@@ -114,17 +116,37 @@ private
 	end
 
 	##
+	# Вызывается из меню
+	def on_action_delete_files_triggered
+		delete_selected_files @ui.treeWidget_2
+		#tab = @ui.tabWidget.tabText(@ui.tabWidget.currentIndex)
+		#case tab
+		#	when 'Compare'
+		#		delete_selected_files @ui.treeWidget
+		#	when 'Find'
+		#		delete_selected_files @ui.treeWidget_2
+		#	else
+		#		pp tab
+		#end
+	end
+
+
+	##
 	# Сравнение двух каталогов
 	def compare_folders
 		folder1 = @ui.lineEdit.text
 		folder2 = @ui.lineEdit_2.text
+
+		log 'compare_folders'
+		log folder1
+		log folder2
 
 		@ui.treeWidget.clear
 
 		@finder.compare(folder1, folder2) do |files1, files2|
 			pp [files1, files2]
 			pp '------------------------'
-			file_size = files1[0].size.to_s
+			file_size = files1[0].size.to_s.rjust(12)
 			parent_it = nil
 
 			max_index = [files1.count, files2.count ].max
@@ -132,12 +154,11 @@ private
 				text1 = "#{files1[i]}"
 				text2 = "#{files2[i]}"
 
-				columns = [ text1, text2, file_size ]
-
 				if parent_it
-					Qt::TreeWidgetItem.new parent_it, columns
+					Qt::TreeWidgetItem.new parent_it, [nil, text1, text2]
 				else
-					parent_it = Qt::TreeWidgetItem.new @ui.treeWidget, columns
+					parent_it = Qt::TreeWidgetItem.new @ui.treeWidget, [file_size, text1, text2]
+					parent_it.setTextAlignment 0, Qt::AlignRight
 					parent_it.setExpanded true
 				end
 
@@ -151,6 +172,9 @@ private
 	def find_in_folder
 		folder = @ui.lineEdit_3.text
 
+		log 'find_in_folder'
+		log folder
+
 		@ui.treeWidget_2.clear
 		@finder.run(folder) do |dup_files|
 			pp dup_files
@@ -159,12 +183,54 @@ private
 			parent_it = nil
 			dup_files.each do |file|
 				if parent_it
-					Qt::TreeWidgetItem.new parent_it, [ nil, file.to_s ]
+					Qt::TreeWidgetItem.new parent_it, [nil, file.to_s]
 				else
-					parent_it = Qt::TreeWidgetItem.new @ui.treeWidget_2, [ file.size.to_s, file.to_s ]
+					size = file.size.to_s.rjust(12)
+					parent_it = Qt::TreeWidgetItem.new @ui.treeWidget_2, [size, file.to_s]
+					parent_it.setTextAlignment 0, Qt::AlignRight
 					parent_it.setExpanded true
 				end
 			end
+		end
+	end
+
+	##
+	# Выбрать каталог
+	def select_folder(control)
+		dir_name = Qt::FileDialog::getExistingDirectory(self, 'Select folder', control.text, Qt::FileDialog::ShowDirsOnly)
+		if dir_name
+			control.text = dir_name
+		end
+	end
+
+	def on_toolButton_clicked
+		select_folder @ui.lineEdit
+	end
+
+	def on_toolButton_2_clicked
+		select_folder @ui.lineEdit_2
+	end
+
+	def on_toolButton_3_clicked
+		select_folder @ui.lineEdit_3
+	end
+
+	##
+	# Удалить файлы выбранные в таблице результатов поиска
+	def delete_selected_files(table)
+		until (all = table.selectedItems).empty?
+			it = all.first
+			file_path = it.text(1)
+
+			puts "delete: #{file_path}"
+			begin
+				File.delete file_path
+			rescue => ex
+				pp ex
+			end
+
+			# Удалить из таблицы
+			it.dispose
 		end
 	end
 
@@ -188,11 +254,6 @@ private
 
 	##
 	#
-	def on_toolButton_clicked
-	end
-
-	##
-	#
 	def on_comboBox_currentIndexChanged(text)
 	end
 
@@ -211,15 +272,19 @@ private
 			self.restoreGeometry Qt::ByteArray.new(@settings.form_geometry.to_s)
 		end
 
-		if @settings.current_dir
-			add_folder @settings.current_dir
-		end
+		@ui.lineEdit.text = @settings.folder1
+		@ui.lineEdit_2.text = @settings.folder2
+		@ui.lineEdit_3.text = @settings.folder3
 	end
 
 	##
 	# Сохранение настроек
 	def save_settings
 		@settings.form_geometry = self.saveGeometry.to_s
+
+		@settings.folder1 = @ui.lineEdit.text
+		@settings.folder2 = @ui.lineEdit_2.text
+		@settings.folder3 = @ui.lineEdit_3.text
 	end
 
 end
